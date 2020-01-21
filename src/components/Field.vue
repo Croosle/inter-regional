bf95e8a540aed859b3fe0b4ef2a04d47a91cf166
alekseypirogov <iartifical@gmail.com>
2020-01-21 18:51:10 +0700

    release

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,54 +1,46 @@
 <template lang="html">
   <div class="field">
-    <div class="row field__row">
-      <label class="custom-label" for="color-select">Цвет:</label>
-      <select class="custom-select" v-model="selected.color" id="color-select">
-        <option value="black">Black</option>
-      </select>
-      <label class="custom-label" for="family-select">Шрифт:</label>
-      <select class="custom-select" v-model="selected.family" id="family-select">
-        <option value="arial">Arial</option>
-      </select>
-      <label class="custom-label" for="size-select">Размер:</label>
-      <select class="custom-select" v-model="selected.size" id="size-select">
-        <option value="13">13 px</option>
-      </select>
-    </div>
-    <input class="custom-input" v-model="text"/>
+    <FormattingPanel v-model="styles"/>
+
+    <textarea
+      class="custom-textarea"
+      placeholder="Введите текст"
+      v-model="text"
+      :rows="lineCount"
+      :style="styles"
+    ></textarea>
   </div>
 </template>
 
 <script>
+import FormattingPanel from './FormattingPanel.vue'
+
 export default {
+  components: {
+    FormattingPanel
+  },
   data () {
     return {
-      selected: {
+      styles: {
         color: 'black',
-        family: 'arial',
-        size: 13
+        fontFamily: 'arial',
+        fontSize: '16px'
       },
       text: ''
+    }
+  },
+  computed: {
+    lineCount () {
+      const length = this.text.split(/\r\n|\r|\n/).length
+
+      return length < 10 ? length : 10
     }
   }
 }
 </script>
 
 <style lang="css" scoped>
-  .row {
-    display: flex;
-  }
-
-  .field__row {
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 1rem;
-  }
-
-  .custom-label {
-    padding: 10px;
-  }
-
-  .custom-input {
+  .custom-textarea {
     display: block;
     width: 100%;
     padding: .375rem .75rem;
@@ -61,31 +53,13 @@ export default {
     border: 1px solid #ced4da;
     border-radius: .25rem;
     transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    resize: none;
+    box-sizing: border-box;
   }
 
-  .custom-input:focus {
+  .custom-textarea:focus {
     color: #495057;
     background-color: #fff;
-    border-color: #80bdff;
-    outline: 0;
-    box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);
-  }
-
-  .custom-select {
-    display: inline-block;
-    width: 100%;
-    height: calc(1.5em + .75rem + 2px);
-    padding: .375rem 1.75rem .375rem .75rem;
-    font-size: 1rem;
-    font-weight: 400;
-    line-height: 1.5;
-    color: #495057;
-    vertical-align: middle;
-    border: 1px solid #ced4da;
-    border-radius: .25rem;
-  }
-
-  .custom-select:focus {
     border-color: #80bdff;
     outline: 0;
     box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);
---
+++ src/components/FormattingPanel.vue
@@ -0,0 +1,78 @@
+<template lang="html">
+  <div class="row">
+    <label class="custom-label" for="color-select">Цвет:</label>
+    <select class="custom-select" v-model="value.color" id="color-select">
+      <option value="black">Black</option>
+      <option value="red">Red</option>
+      <option value="blue">Blue</option>
+    </select>
+
+    <label class="custom-label" for="family-select">Шрифт:</label>
+    <select class="custom-select" v-model="value.fontFamily" id="family-select">
+      <option value="arial">Arial</option>
+      <option value="courier">Courier</option>
+      <option value="impact">Impact</option>
+    </select>
+
+    <label class="custom-label" for="size-select">Размер:</label>
+    <select class="custom-select" v-model="value.fontSize" id="size-select">
+      <option value="15px">15 px</option>
+      <option value="16px">16 px</option>
+      <option value="17px">17 px</option>
+      <option value="18px">18 px</option>
+    </select>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['value']
+  }
+</script>
+
+<style lang="css" scoped>
+  .row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+  }
+
+  .custom-label {
+    padding: 10px;
+    font-size: 16px;
+    font-family: Arial;
+    color: #495057;
+  }
+
+  .custom-label:first-child {
+    padding-left: 0;
+  }
+
+  .custom-select {
+    display: inline-block;
+    width: 100%;
+    height: calc(1.5em + .75rem + 2px);
+    padding: .375rem 1.75rem .375rem .75rem;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    vertical-align: middle;
+    border: 1px solid #ced4da;
+    border-radius: .25rem;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='4' height='5' viewBox='0 0 4 5'%3e%3cpath fill='%23343a40' d='M2 0L0 2h4zm0 5L0 3h4z'/%3e%3c/svg%3e");
+    background-repeat: no-repeat;
+    background-size: 8px 10px;
+    background-position: right .75rem center;
+  }
+
+  .custom-select:focus {
+    border-color: #80bdff;
+    outline: 0;
+    box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);
+  }
+</style>
