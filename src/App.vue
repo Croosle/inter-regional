e486f3ec682021b81ecf7235403b1c0e309a513d
alekseypirogov <iartifical@gmail.com>
2020-01-21 17:06:50 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Field/>   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Field from './components/Field.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Field
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .container {
+    max-width: 1170px;
+    margin: 0px auto;
+  }
 </style>
---
+++ src/components/Field.vue
@@ -0,0 +1,93 @@
+<template lang="html">
+  <div class="field">
+    <div class="row field__row">
+      <label class="custom-label" for="color-select">Цвет:</label>
+      <select class="custom-select" v-model="selected.color" id="color-select">
+        <option value="black">Black</option>
+      </select>
+      <label class="custom-label" for="family-select">Шрифт:</label>
+      <select class="custom-select" v-model="selected.family" id="family-select">
+        <option value="arial">Arial</option>
+      </select>
+      <label class="custom-label" for="size-select">Размер:</label>
+      <select class="custom-select" v-model="selected.size" id="size-select">
+        <option value="13">13 px</option>
+      </select>
+    </div>
+    <input class="custom-input" v-model="text"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      selected: {
+        color: 'black',
+        family: 'arial',
+        size: 13
+      },
+      text: ''
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  .row {
+    display: flex;
+  }
+
+  .field__row {
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+  }
+
+  .custom-label {
+    padding: 10px;
+  }
+
+  .custom-input {
+    display: block;
+    width: 100%;
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: .25rem;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  }
+
+  .custom-input:focus {
+    color: #495057;
+    background-color: #fff;
+    border-color: #80bdff;
+    outline: 0;
+    box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);
+  }
+
+  .custom-select {
+    display: inline-block;
+    width: 100%;
+    height: calc(1.5em + .75rem + 2px);
+    padding: .375rem 1.75rem .375rem .75rem;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    vertical-align: middle;
+    border: 1px solid #ced4da;
+    border-radius: .25rem;
+  }
+
+  .custom-select:focus {
+    border-color: #80bdff;
+    outline: 0;
+    box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);
+  }
+</style>
